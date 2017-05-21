084e11db53a60b373ace3f7631038321ff9f5ba9
Shyanta Vleugel <shyantavleugel@gmail.com>
2017-05-21 19:12:42 +0200

    Delete counter for localStorage

--- final-exersize/public/js/scripts.js
+++ final-exersize/public/js/scripts.js
@@ -5,18 +5,17 @@ var shirtColor = document.querySelector('img').getAttribute('alt');
 var style = document.querySelector('p').getAttribute('style');
 var savedSection = document.querySelector('.saved-items');
 var savedNoItems = document.querySelector('.saved-items p');
-var num;
+	savedSection.setAttribute('hidden', true);
+	saveButton.setAttribute('hidden', true);
 
 if (typeof window.localStorage !== "undefined"){
 	savedSection.removeAttribute('hidden');
 	saveButton.removeAttribute('hidden');
 	saveButton.addEventListener('click', function(){
 
-		localStorage.setItem('style'+num, style);
-		localStorage.setItem('text'+num, text);
-		localStorage.setItem('color'+num, shirtColor);
-		num ++;
-		console.log(num);
+		localStorage.setItem('style', style);
+		localStorage.setItem('text', text);
+		localStorage.setItem('color', shirtColor);
 	})
 
 	if (window.localStorage.length == 0) {
