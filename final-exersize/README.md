36d92abcc3abdd993ad02195f913de79bc9cee58
Shyanta Vleugel <shyantavleugel@gmail.com>
2017-05-24 06:56:13 +0200

    Add HTML and CSS Enhancements

--- final-exersize/README.md
+++ final-exersize/README.md
@@ -1,1 +1,17 @@
-# Ik wil favoriete t-shirts-met-nerdy-teksten kunnen opslaan, en een volgende keer dat ik de site bezoek kunnen gebruiken
+# minor-BrowserTechnologies
+
+### Case:
+Ik wil favoriete t-shirts-met-nerdy-teksten kunnen opslaan, en een volgende keer dat ik de site bezoek kunnen gebruiken
+I want to save my favorite T-shirts-with-nerdy-texts, and want to use them the next time I visit
+
+## Tests
+
+## Features
+
+## Progressive Enhancements
+-	localStorage
+-	Flexbox
+
+## Backwards Compatible items
+-	querySelector
+-	HTML 5 Elements
--- final-exersize/public/js/scripts.js
+++ final-exersize/public/js/scripts.js
@@ -1,74 +1,129 @@
-var saveButton1 = document.querySelector('.button-first');
-var saveButton2 = document.querySelector('.button-second');
-var saveButton3 = document.querySelector('.button-third');
+var saveButton1 = document.getElementById('button-first');
+var saveButton2 = document.getElementById('button-second');
+var saveButton3 = document.getElementById('button-third');
 
-var shirtText = document.querySelector('p').innerHTML;
-var shirtColor = document.querySelector('img').getAttribute('alt');
-var shirtStyle = document.querySelector('p').getAttribute('style');
-var savedSection = document.querySelector('.saved-items');
-var savedSectionList = document.querySelector('.saved-shirts');
-var savedFirst = document.querySelector('.shirtOne');
-var savedSecond = document.querySelector('.shirtOne');
-var savedThird = document.querySelector('.shirtThree');
-var savedNoItems = document.querySelector('.saved-items p');
+var shirtText = document.getElementById('shirt-text').innerHTML;
+var shirtColor = document.getElementById('img').getAttribute('alt');
+var shirtStyle = document.getElementById('shirt-text').getAttribute('style');
+var savedSection = document.getElementById('saved-items');
+var savedSectionList = document.getElementById('saved-shirts');
+var savedFirst = document.getElementById('shirtOne');
+var savedSecond = document.getElementById('shirtTwo');
+var savedThird = document.getElementById('shirtThree');
+var savedNoItems = document.getElementById('no-shirts');
 	savedSection.setAttribute('hidden', true);
 	saveButton1.setAttribute('hidden', true);
 	saveButton2.setAttribute('hidden', true);
 	saveButton3.setAttribute('hidden', true);
 
-if (typeof window.localStorage !== "undefined"){
+if (typeof window.localStorage !== "undefined" || JSON.parse(localStorage.getItem('shirt1')) === ""){
 	savedSection.removeAttribute('hidden');
 	saveButton1.removeAttribute('hidden');
 	saveButton2.removeAttribute('hidden');
 	saveButton3.removeAttribute('hidden');
 
-	saveButton1.addEventListener('click', function(){
-		var shirtObject1 = {
-				'color': shirtColor,
-				'style': shirtStyle,
-				'text': shirtText
-			}
-		console.log(shirtObject1);
-		localStorage.setItem('shirt1', JSON.stringify(shirtObject1))
-	})
-	saveButton2.addEventListener('click', function(){
-		var shirtObject2 = {
-				'color': shirtColor,
-				'style': shirtStyle,
-				'text': shirtText
-			}
-		console.log(shirtObject2);
-		localStorage.setItem('shirt2', JSON.stringify(shirtObject2))
-	})
-	saveButton3.addEventListener('click', function(){
-		var shirtObject3 = {
-				'color': shirtColor,
-				'style': shirtStyle,
-				'text': shirtText
-			}
-		console.log(shirtObject3);
-		localStorage.setItem('shirt3', JSON.stringify(shirtObject3))
-	})
-
+	if(document.addEventListener){
+		saveButton1.addEventListener('click', function(){
+			var shirtObject1 = {
+					'color': shirtColor,
+					'style': shirtStyle,
+					'text': shirtText
+				}
+			console.log(shirtObject1);
+			localStorage.setItem('shirt1', JSON.stringify(shirtObject1));
+			shirtOneFunc(JSON.parse(localStorage.getItem('shirt1')));
+		})
+		saveButton2.addEventListener('click', function(){
+			var shirtObject2 = {
+					'color': shirtColor,
+					'style': shirtStyle,
+					'text': shirtText
+				}
+			console.log(shirtObject2);
+			localStorage.setItem('shirt2', JSON.stringify(shirtObject2));
+			shirtTwoFunc(JSON.parse(localStorage.getItem('shirt2')));
+		})
+		saveButton3.addEventListener('click', function(){
+			var shirtObject3 = {
+					'color': shirtColor,
+					'style': shirtStyle,
+					'text': shirtText
+				}
+			console.log(shirtObject3);
+			localStorage.setItem('shirt3', JSON.stringify(shirtObject3));
+			shirtThreeFunc(JSON.parse(localStorage.getItem('shirt3')));
+		})
+	} else {
+		saveButton1.attachEvent('onclick', function(){
+			var shirtObject1 = {
+					'color': shirtColor,
+					'style': shirtStyle,
+					'text': shirtText
+				}
+			console.log(shirtObject1);
+			localStorage.setItem('shirt1', JSON.stringify(shirtObject1));
+			shirtOneFunc(JSON.parse(localStorage.getItem('shirt1')));
+		})
+		saveButton2.attachEvent('onclick', function(){
+			var shirtObject2 = {
+					'color': shirtColor,
+					'style': shirtStyle,
+					'text': shirtText
+				}
+			console.log(shirtObject2);
+			localStorage.setItem('shirt2', JSON.stringify(shirtObject2));
+			shirtTwoFunc(JSON.parse(localStorage.getItem('shirt2')));
+		})
+		saveButton3.attachEvent('onclick', function(){
+			var shirtObject3 = {
+					'color': shirtColor,
+					'style': shirtStyle,
+					'text': shirtText
+				}
+			console.log(shirtObject3);
+			localStorage.setItem('shirt3', JSON.stringify(shirtObject3));
+			shirtThreeFunc(JSON.parse(localStorage.getItem('shirt3')));
+		});
+	}
+	console.log(JSON.parse(localStorage.getItem('shirt1')));
 	var shirtOneData = JSON.parse(localStorage.getItem('shirt1'));
 	var shirtTwoData = JSON.parse(localStorage.getItem('shirt2'));
 	var shirtThreeData = JSON.parse(localStorage.getItem('shirt3'));
-	
+
 	if (shirtOneData === null && shirtTwoData === null && shirtThreeData === null) {
 		savedNoItems.innerHTML = "You don't have any saved items yet";
 		savedSectionList.setAttribute('hidden', true);
 	} else {
 		savedSectionList.removeAttribute('hidden');
+		// console.log(savedFirst.innerHTML);
+		if (savedFirst.innerHTML === ""){
 
+			shirtOneFunc(shirtOneData);
+		}
+		if (savedSecond.innerHTML === ""){
+			shirtTwoFunc(shirtTwoData);
+		}
+		if (savedThird.innerHTML === ""){
+			shirtThreeFunc(shirtThreeData);
+		}
+	}
+
+	function shirtOneFunc(shirtOneData){
 		if (shirtOneData !== null){
 			savedFirst.innerHTML = '<p style="'+shirtOneData.style+'">'+shirtOneData.text
-			+'<img src="img/shirt-'+shirtOneData.color+'.jpg" alt="'+shirtOneData.color+'" title="'+shirtOneData.color+'"/></p>'
-		}else if (shirtTwoData !== null){
+			+'</p><img src="img/shirt-'+shirtOneData.color+'.jpg" alt="'+shirtOneData.color+'" title="'+shirtOneData.color+'"/>'
+		}
+	}
+	function shirtTwoFunc(shirtTwoData){
+		if (shirtTwoData !== null){
 			savedSecond.innerHTML = '<p style="'+shirtTwoData.style+'">'+shirtTwoData.text
-			+'<img src="img/shirt-'+shirtTwoData.color+'.jpg" alt="'+shirtTwoData.color+'" title="'+shirtTwoData.color+'"/></p>'
-		}else if (shirtThreeData !== null){
+			+'</p><img src="img/shirt-'+shirtTwoData.color+'.jpg" alt="'+shirtTwoData.color+'" title="'+shirtTwoData.color+'"/>'
+		}
+	}
+	function shirtThreeFunc(shirtThreeData){
+		if (shirtThreeData !== null){
 			savedThird.innerHTML = '<p style="'+shirtThreeData.style+'">'+shirtThreeData.text
-			+'<img src="img/shirt-'+shirtThreeData.color+'.jpg" alt="'+shirtThreeData.color+'" title="'+shirtThreeData.color+'"/></p>'
+			+'</p><img src="img/shirt-'+shirtThreeData.color+'.jpg" alt="'+shirtThreeData.color+'" title="'+shirtThreeData.color+'"/>'
 		}
 	}
 }
