3dca2731f5c2601a118561ad128db7a9832d8a84
Shyanta Vleugel <shyantavleugel@gmail.com>
2017-05-24 02:04:54 +0200

    Add LocalStorage save

--- final-exersize/public/js/scripts.js
+++ final-exersize/public/js/scripts.js
@@ -1,26 +1,74 @@
-var saveButton = document.querySelector('button');
+var saveButton1 = document.querySelector('.button-first');
+var saveButton2 = document.querySelector('.button-second');
+var saveButton3 = document.querySelector('.button-third');
 
-var text = document.querySelector('p').innerHTML;
+var shirtText = document.querySelector('p').innerHTML;
 var shirtColor = document.querySelector('img').getAttribute('alt');
-var style = document.querySelector('p').getAttribute('style');
+var shirtStyle = document.querySelector('p').getAttribute('style');
 var savedSection = document.querySelector('.saved-items');
+var savedSectionList = document.querySelector('.saved-shirts');
+var savedFirst = document.querySelector('.shirtOne');
+var savedSecond = document.querySelector('.shirtOne');
+var savedThird = document.querySelector('.shirtThree');
 var savedNoItems = document.querySelector('.saved-items p');
 	savedSection.setAttribute('hidden', true);
-	saveButton.setAttribute('hidden', true);
+	saveButton1.setAttribute('hidden', true);
+	saveButton2.setAttribute('hidden', true);
+	saveButton3.setAttribute('hidden', true);
 
 if (typeof window.localStorage !== "undefined"){
 	savedSection.removeAttribute('hidden');
-	saveButton.removeAttribute('hidden');
-	saveButton.addEventListener('click', function(){
+	saveButton1.removeAttribute('hidden');
+	saveButton2.removeAttribute('hidden');
+	saveButton3.removeAttribute('hidden');
 
-		localStorage.setItem('style', style);
-		localStorage.setItem('text', text);
-		localStorage.setItem('color', shirtColor);
+	saveButton1.addEventListener('click', function(){
+		var shirtObject1 = {
+				'color': shirtColor,
+				'style': shirtStyle,
+				'text': shirtText
+			}
+		console.log(shirtObject1);
+		localStorage.setItem('shirt1', JSON.stringify(shirtObject1))
+	})
+	saveButton2.addEventListener('click', function(){
+		var shirtObject2 = {
+				'color': shirtColor,
+				'style': shirtStyle,
+				'text': shirtText
+			}
+		console.log(shirtObject2);
+		localStorage.setItem('shirt2', JSON.stringify(shirtObject2))
+	})
+	saveButton3.addEventListener('click', function(){
+		var shirtObject3 = {
+				'color': shirtColor,
+				'style': shirtStyle,
+				'text': shirtText
+			}
+		console.log(shirtObject3);
+		localStorage.setItem('shirt3', JSON.stringify(shirtObject3))
 	})
 
-	if (window.localStorage.length == 0) {
+	var shirtOneData = JSON.parse(localStorage.getItem('shirt1'));
+	var shirtTwoData = JSON.parse(localStorage.getItem('shirt2'));
+	var shirtThreeData = JSON.parse(localStorage.getItem('shirt3'));
+	
+	if (shirtOneData === null && shirtTwoData === null && shirtThreeData === null) {
 		savedNoItems.innerHTML = "You don't have any saved items yet";
+		savedSectionList.setAttribute('hidden', true);
 	} else {
-		// saveButton.eventListener('')
+		savedSectionList.removeAttribute('hidden');
+
+		if (shirtOneData !== null){
+			savedFirst.innerHTML = '<p style="'+shirtOneData.style+'">'+shirtOneData.text
+			+'<img src="img/shirt-'+shirtOneData.color+'.jpg" alt="'+shirtOneData.color+'" title="'+shirtOneData.color+'"/></p>'
+		}else if (shirtTwoData !== null){
+			savedSecond.innerHTML = '<p style="'+shirtTwoData.style+'">'+shirtTwoData.text
+			+'<img src="img/shirt-'+shirtTwoData.color+'.jpg" alt="'+shirtTwoData.color+'" title="'+shirtTwoData.color+'"/></p>'
+		}else if (shirtThreeData !== null){
+			savedThird.innerHTML = '<p style="'+shirtThreeData.style+'">'+shirtThreeData.text
+			+'<img src="img/shirt-'+shirtThreeData.color+'.jpg" alt="'+shirtThreeData.color+'" title="'+shirtThreeData.color+'"/></p>'
+		}
 	}
 }
